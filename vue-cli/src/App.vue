20725526aa5359616307a9d384e35af4940d93c7
GBU_DanhNC <danh.nguyen@dsquare.com.vn>
2021-01-19 17:50:50 +0700

    add an cli-structure project

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+    #app
+        img(src="./assets/logo.png" alt="Vue Logo")
+        base-header
+
+</template>
+
+<script>
+
+import BaseHeader from "@/components/BaseHeader";
+
+export default {
+    name: 'App',
+    components: {
+        BaseHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/src/components/BaseHeader.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+
+</template>
+
+<script>
+export default {
+    name: 'base-header',
+}
+</script>
+
+<style scoped>
+
+</style>
