57e4ff4674b44399b003f831e30d126529b6dc10
GBU_DanhNC <danh.nguyen@dsquare.com.vn>
2021-01-19 18:03:14 +0700

    add package

--- vue-cli/src/components/BaseHeader.vue
+++ vue-cli/src/components/BaseHeader.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
-
+    p.align-content-centers.text-black-50 {{ message }}
 </template>
 
 <script>
 export default {
     name: 'base-header',
+    data() {
+        return {
+            message: 'My first Cli-Vue project'
+        }
+    }
 }
 </script>
 
