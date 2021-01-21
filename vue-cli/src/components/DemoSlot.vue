b93a1f259528fc04966dbef2927cf0b768fa7dad
GBU_DanhNC <danh.nguyen@dsquare.com.vn>
2021-01-21 10:27:05 +0700

    add

--- vue-cli/src/components/DemoSlot.vue
+++ vue-cli/src/components/DemoSlot.vue
@@ -1,15 +1,54 @@
 <template lang="pug">
-    div
+    div.demo-slot
         h2 This is a Vue slot
         slot
+        tab( :title="title" )
+        ul.tabs-header
+            li(v-for="(item, index) in tabsHeaders" :key="index") {{ item.text }}
 </template>
 
 <script>
+import Tab from "@/components/Tab";
+
 export default {
-    name: "demo-slot"
+    name: "demo-slot",
+    components: {
+        Tab,
+    },
+    data() {
+        return {
+            title: 'Tab title',
+            tabsHeaders: [
+                { text: 'Tab1' },
+                { text: 'Tab2' },
+                { text: 'Tab3' },
+            ]
+        }
+    }
+
 }
 </script>
 
 <style scoped>
-
+    .demo-slot {
+        margin-top: 100px;
+        padding: 100px 30px;
+        border: 1px solid #333333;
+    }
+    .tabs-header {
+        border: 2px solid darkblue;
+        margin: 2px;
+        padding: 2px;
+        display: flex;
+        list-style-type: none;
+    }
+    .tabs-header li {
+        padding: 8px 15px;
+        background-color: #ccc;
+        transition: all 0.3s;
+    }
+    .tabs-header li:hover {
+        background-color: gray;
+        color: white;
+    }
 </style>
--- vue-cli/src/components/Tab.vue
+++ vue-cli/src/components/Tab.vue
@@ -1,17 +1,35 @@
 <template lang="pug">
-
+    .comp-tabs
+        .tabs-title
+            h2 {{ title }}
+            i.icon.ion-md-heart
+        .tabs-content
+            slot
 </template>
 
 <script>
 export default {
     name: "tab",
+    props: {
+        title: {
+            type: String,
+            default: '',
+        }
+    },
     data() {
         return {}
     },
-    methods: {},
+    methods: {
+
+    },
 }
 </script>
 
 <style scoped>
-
+    .tabs-title {
+        border: 2px solid #333;
+        padding: 15px 30px;
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
